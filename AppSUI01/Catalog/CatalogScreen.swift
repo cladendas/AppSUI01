376e3e001b41c6485f6b0d21713e3a5aba89b8eb
Vadim <rvdagi05@yandex.ru>
2021-12-13 20:03:53 +0300

    добавил Picker в CatalogScreen

--- AppSUI01/Catalog/CatalogScreen.swift
+++ AppSUI01/Catalog/CatalogScreen.swift
@@ -17,8 +17,34 @@ struct CatalogScreen: View {
 
 struct FoodList: View {
     @EnvironmentObject var foodListViewModel: FoodListViewModel
+    @State var listViewTypeChoise: Int = 0
+    var listTypes = ["List", "Lazy Grid", "Grid iOS 13"]
     
     var body: some View {
+        ScrollView {
+            VStack {
+                Picker("Lists", selection: $listViewTypeChoise) {
+                    ForEach(0..<listTypes.count) { index in
+                        Text(listTypes[index])
+                            .tag(index)
+                    }
+                    
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                if listViewTypeChoise == 0 {
+                    list
+                } else if listViewTypeChoise == 1 {
+                    list
+                } else {
+                    list
+                }
+            }
+            .frame(minHeight: 1000)
+        }
+    }
+    
+    var list: some View {
         List {
             CatalogFilterCell()
                 .environmentObject(foodListViewModel)
