e8746ef8599d9d528f4f72f7581d65e8a93097b1
Vadim <rvdagi05@yandex.ru>
2021-12-13 16:52:44 +0300

    добавил код, позволяющий наблюдать за терминальным состоянием приложения

--- AppSUI01/AppSUI01App.swift
+++ AppSUI01/AppSUI01App.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 @main
 struct AppSUI01App: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environmentObject(DashboardViewModel())
+//        }
+//    }
+    
+    
+    //MARK: код ниже позволите смотреть за терминальным состоянием приложения
+    @Environment(\.scenePhase) var scenePhase
+    
+    init() {
+      print("📟 AppSUIApp application is starting up. App initialiser.")
+    }
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environmentObject(DashboardViewModel())
+      WindowGroup {
+          ContentView()
+              .environmentObject(DashboardViewModel())
+      }
+      .onChange(of: scenePhase) { newScenePhase in
+        switch newScenePhase {
+        case .active:
+          print("📟 App is active")
+        case .inactive:
+          print("📟 App is inactive")
+        case .background:
+          print("📟 App is in background")
+        @unknown default:
+          print("📟 Oh - interesting: I received an unexpected new value.")
         }
+      }
     }
 }
